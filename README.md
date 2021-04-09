c8c5643e596abab4ddba0a17083106f4df1bf5cb
Jawad W. Ahmed <Jawad.W.Ahmed@gmail.com>
2021-04-09 22:28:43 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# YT-CLI
+
+A YouTube viewer for the command line
+
+## How to use:
+
+### Prerequisites:
+
+- A "True Colour" capable terminal, run the following command to check (it should output the word "TRUECOLOUR" in orange):
+```sh
+echo "\x1b[38;2;255;100;0mTRUECOLOR\x1b[0m\n"
+```
+- [youtube-dl](https://github.com/ytdl-org/youtube-dl#installation) - Downloads the video
+- [ffmpeg](https://ffmpeg.org/download.html) - Performs the conversion from video to images
+- [pillow](https://pillow.readthedocs.io/en/stable/installation.html) - Used to read the image files
+
+### How to run:
+
+Clone the repository and then cd into the folder and run `./yt-cli.py --url <video url>`. _**Make sure to use it in a directory where there are no sub-directories named `video` or `images` because the script recursively deletes the folders while running**_
+
+## How it works:
+
+The python script uses `youtube-dl` to download the video to `video/` (in the current directory), converts said video to image frames using `ffmpeg` (`.bmp`) in `images/` (in the current directory), loads all of the frames into a List with each element being a List of Tuples which each represent a pixel (using `pillow`). This list of frames is then looped through and each frame is printed out as a string containing unicode block characters (for each pixel) coloured using ANSI escape codes (24 bit colour, works in most modern terminals).
---
+++ yt-cli.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import time
+import shutil
+import subprocess
+from typing import List, Tuple
+from PIL import Image
+
+
+def main():
+    url = get_flag('url')
+    if url == '':
+        print('Usage: ./yt-cli.py {-u/--url} <url>')
+
+    frames = get_frames(url)
+
+    print('\x1b[38;2;0;128;0mStarting video...\x1b[0m\n')
+    time.sleep(0.5)
+    draw(frames)
+
+
+def draw(frames: List[List[Tuple[int, int, int]]]):
+    print('\x1b[2J')
+    for fr in frames:
+        frame = ''
+        for i in range(0, 4608):
+            val = fr[i]
+            red, green, blue = val
+            frame += f'\x1b[38;2;{red};{green};{blue}m█\x1b[0m'
+
+            # Speedy modulo :)
+            if not ((i + 1) & 127):
+                frame += '\n'
+
+        time.sleep(0.08)
+        print('\x1b[2J')
+        print(frame)
+
+
+def get_frames(url: str) -> List[List[Tuple[int, int, int]]]:
+    os.mkdir('./images')
+    os.mkdir('./video')
+
+    print(
+        '\x1b[38;2;0;128;0mDownloading video and converting...\x1b[0m',
+    )
+    subprocess.run([
+        'youtube-dl',
+        '--no-continue',
+        '--format', 'worst',
+        '--external-downloader', 'ffmpeg',
+        '--external-downloader-args',
+        '-s 128x36 -r 10 -pix_fmt bgr24 ./images/frame_%04d.bmp',
+        '--output', './video/%(id)s.%(ext)s',
+        url
+    ], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+    print('\x1b[38;2;0;128;0mReading frames...\x1b[0m')
+    frames = []
+    for name in sorted(os.listdir('./images')):
+        with Image.open(f'./images/{name}', 'r') as f:
+            frames.append(list(f.getdata()))
+
+    shutil.rmtree('./images')
+    shutil.rmtree('./video')
+    return frames
+
+
+def get_flag(flag: str) -> str:
+    for i in range(len(sys.argv) - 1):
+        if sys.argv[i] == '--' + flag or sys.argv[i] == '-' + flag[0]:
+            return sys.argv[i + 1]
+
+    return ''
+
+
+if __name__ == '__main__':
+    main()
